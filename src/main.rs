1d66bd39a70749f65970052695b95d2cff45fe1f
Mauricio <mcimauricio.coelho@gmail.com>
2024-04-30 12:46:46 +0200

    deploy to netlify

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use lambda_http::{http::header::CONTENT_TYPE, run, service_fn, Body, Error, Request, Response};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Error> {
+    run(service_fn(function_handler)).await
+}
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    dbg!(event);
+    let html = "<html><body><h1>Hello from Rust serverless!</h1></body></html>";
+    let resp = Response::builder()
+        .status(200)
+        .header(CONTENT_TYPE, "text/html")
+        .body(Body::Text(html.to_string()))?;
+    Ok(resp)
 }
